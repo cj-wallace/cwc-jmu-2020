c88cb3f442d06ddb66f709b87f3c7f24018073c9
Connor Wallace <connorjwallace@hotmail.com>
2019-11-12 09:55:25 -0500

    Commit: Tue, Nov 12, 2019  9:55:25 AM

--- admin/index.php
+++ admin/index.php
@@ -1,24 +1,43 @@
-<?php
-	include("../include/config.php");
-	
-	if ($connection->connect_errno) {
-		printf("Connect failed: %s\n", $connection->connect_error);
-		exit();
-	}
-	$query = "SELECT * FROM admin";
-	if ($results = $connection->query($query)) {
-		printf("%d Admins.\n", $results->num_rows);
-		while($result = $results->fetch_assoc()) {
-			printf("
-				<ul id='%s'>
-					<li class='username'><b>Username:</b> %s</li>
-					<li class='password'><b>Password:</b> %s</li>
-				</ul>",
-				$result["id"],
-				$result["username"],
-				$result["password"]);
+<?php include("../include/config.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include("include/head.php");?>
+</head>
+<body>
+
+<?php include("include/navigation.php");?>
+
+<div class="container" id="main-content">
+	<?php displayQuery() ?>
+</div>
+
+<?php include("include/footer.php");?>
+
+</body>
+</html>
+
+<?php	
+	function displayQuery() {
+		if ($connection->connect_errno) {
+			printf("Connect failed: %s\n", $connection->connect_error);
+			exit();
 		}
-		/* free result set */
-		$results->close();
+		$query = "SELECT * FROM admin";
+		if ($results = $connection->query($query)) {
+			printf("%d Admins.\n", $results->num_rows);
+			while($result = $results->fetch_assoc()) {
+				printf("
+					<ul id='%s'>
+						<li class='username'><b>Username:</b> %s</li>
+						<li class='password'><b>Password:</b> %s</li>
+					</ul>",
+					$result["id"],
+					$result["username"],
+					$result["password"]);
+			}
+			/* free result set */
+			$results->close();
+		}
 	}
 ?>
