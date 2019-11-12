d855d3d4fa650695f5fb49c656f51a0eae280703
Connor Wallace <connorjwallace@hotmail.com>
2019-11-12 10:25:49 -0500

    Commit: Tue, Nov 12, 2019 10:25:49 AM

--- links/index.php
+++ links/index.php
@@ -1,28 +1,43 @@
-<?php
-	include("../include/config.php");
-	
-	if ($connection->connect_errno) {
-		printf("Connect failed: %s\n", $connection->connect_error);
-		exit();
-	}
-	$query = "SELECT * FROM link";
-	if ($results = $connection->query($query)) {
-		printf("%d Links.\n", $results->num_rows);
-		while($result = $results->fetch_assoc()) {
-			printf("
-				<ul id='%s'>
-					<li class='title'><b>Title:</b> %s</li>
-					<li class='link'><b>Link:</b> %s</li>
-					<li class='link_img'><b>Link Image:</b> %s</li>
-					<li class='type'><b>Type:</b> %s</li>
-				</ul>",
-				$result["id"],
-				$result["title"],
-				$result["link"],
-				$result["link_img"],
-				$result["type"]);
-		}
-		/* free result set */
-		$results->close();
-	}
-?>
+<?php include("../include/config.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include(PATH_ROOT . "/include/head.php");?>
+</head>
+<body>
+
+	<?php include(PATH_ROOT . "/include/navigation.php");?>
+
+	<div class="container" id="main-content">
+		<?php			
+			if ($connection->connect_errno) {
+				printf("Connect failed: %s\n", $connection->connect_error);
+				exit();
+			}
+			$query = "SELECT * FROM link";
+			if ($results = $connection->query($query)) {
+				printf("%d Links.\n", $results->num_rows);
+				while($result = $results->fetch_assoc()) {
+					printf("
+						<ul id='%s'>
+							<li class='title'><b>Title:</b> %s</li>
+							<li class='link'><b>Link:</b> %s</li>
+							<li class='link_img'><b>Link Image:</b> %s</li>
+							<li class='type'><b>Type:</b> %s</li>
+						</ul>",
+						$result["id"],
+						$result["title"],
+						$result["link"],
+						$result["link_img"],
+						$result["type"]);
+				}
+				/* free result set */
+				$results->close();
+			}
+		?>
+	</div>
+
+	<?php include(PATH_ROOT . "/include/footer.php");?>
+
+</body>
+</html>
--- team/index.php
+++ team/index.php
@@ -1,35 +1,50 @@
-<?php
-	include("../include/config.php");
-	
-	if ($connection->connect_errno) {
-		printf("Connect failed: %s\n", $connection->connect_error);
-		exit();
-	}
-	$query = "SELECT * FROM member";
-	if ($results = $connection->query($query)) {
-		printf("%d Members.\n", $results->num_rows);
-		while($result = $results->fetch_assoc()) {
-			printf("
-				<ul id='%s'>
-					<li class='name'><b>Name:</b> %s %s</li>
-					<li class='email'><b>Email:</b> %s</li>
-					<li class='major'><b>Major:</b> %s</li>
-					<li class='age'><b>Age:</b> %s</li>
-					<li class='grad_year'><b>Graduation Year:</b> %s</li>
-					<li class='bio'><b>Bio:</b> %s</li>
-					<li class='profile_img'><b>Image:</b> %s</li>
-				</ul>",
-				$result["id"],
-				$result["first_name"],
-				$result["last_name"],
-				$result["email"],
-				$result["major"],
-				$result["age"],
-				$result["grad_year"],
-				$result["bio"],
-				$result["profile_img"]);
-		}
-		/* free result set */
-		$results->close();
-	}
-?>
+<?php include("../include/config.php"); ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<?php include(PATH_ROOT . "/include/head.php");?>
+</head>
+<body>
+
+	<?php include(PATH_ROOT . "/include/navigation.php");?>
+
+	<div class="container" id="main-content">
+		<?php
+			if ($connection->connect_errno) {
+				printf("Connect failed: %s\n", $connection->connect_error);
+				exit();
+			}
+			$query = "SELECT * FROM member";
+			if ($results = $connection->query($query)) {
+				printf("%d Members.\n", $results->num_rows);
+				while($result = $results->fetch_assoc()) {
+					printf("
+						<ul id='%s'>
+							<li class='name'><b>Name:</b> %s %s</li>
+							<li class='email'><b>Email:</b> %s</li>
+							<li class='major'><b>Major:</b> %s</li>
+							<li class='age'><b>Age:</b> %s</li>
+							<li class='grad_year'><b>Graduation Year:</b> %s</li>
+							<li class='bio'><b>Bio:</b> %s</li>
+							<li class='profile_img'><b>Image:</b> %s</li>
+						</ul>",
+						$result["id"],
+						$result["first_name"],
+						$result["last_name"],
+						$result["email"],
+						$result["major"],
+						$result["age"],
+						$result["grad_year"],
+						$result["bio"],
+						$result["profile_img"]);
+				}
+				/* free result set */
+				$results->close();
+			}
+		?>
+	</div>
+
+	<?php include(PATH_ROOT . "/include/footer.php");?>
+
+</body>
+</html>
