b8e26c2d17a81d01f4873b94b5df5ee5641dcb2b
Connor Wallace <connorjwallace@hotmail.com>
2019-11-12 00:08:44 -0500

    Commit: Tue, Nov 12, 2019 12:08:44 AM

--- db/db.php
+++ db/db.php
@@ -1,5 +1,4 @@
 <?php
-
 $db_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
 
 $db_server = $db_url["host"];
@@ -10,7 +9,6 @@ $db = substr($db_url["path"], 1);
 $connection = new mysqli($db_server, $db_username, $db_password, $db);
 
 $db_connection_status;
-
 
 if ($connection->connect_errno) {
         $db_connection_status = "Failed to connect to MySQL: (" . $connection->connect_errno . ") " . $connection->connect_error;
--- team/index.php
+++ team/index.php
@@ -1,44 +1,38 @@
 <?php
 	include '../db/db.php';
 
-	query();
-
-
-	function query() {
-					global $connection;
-					date_default_timezone_set('America/New_York');
-					$today = date(DATE_RSS);
-					$query = "select * from member";
-					if ($connection->connect_errno) {
-							printf("Connect failed: %s\n", $connection->connect_error);
-							exit();
-					}
-					if ($results = $connection->query($query)) {
-							printf("Select returned %d rows.\n", $results->num_rows);
-							while($result = $results->fetch_assoc()) {
-									printf("
-										<ul id='%s'>
-											<li class='name'><b>Name:</b> %s %s</li>
-											<li class='email'><b>Email:</b> %s</li>
-											<li class='major'><b>Major:</b> %s</li>
-											<li class='age'><b>Age:</b> %s</li>
-											<li class='grad_year'><b>Graduation Year:</b> %s</li>
-											<li class='bio'><b>Bio:</b> %s</li>
-											<li class='profile_img'><b>Image:</b> %s</li>
-										</ul>",
-										$result["id"],
-										$result["first_name"],
-										$result["last_name"],
-										$result["email"],
-										$result["major"],
-										$result["age"],
-										$result["grad_year"],
-										$result["bio"],
-										$result["profile_img"]);
-							}
-							/* free result set */
-							$results->close();
-					}
-			}
-
+	global $connection;
+	date_default_timezone_set('America/New_York');
+	$today = date(DATE_RSS);
+	$query = "select * from member";
+	if ($connection->connect_errno) {
+		printf("Connect failed: %s\n", $connection->connect_error);
+		exit();
+	}
+	if ($results = $connection->query($query)) {
+		printf("Select returned %d rows.\n", $results->num_rows);
+		while($result = $results->fetch_assoc()) {
+			printf("
+				<ul id='%s'>
+					<li class='name'><b>Name:</b> %s %s</li>
+					<li class='email'><b>Email:</b> %s</li>
+					<li class='major'><b>Major:</b> %s</li>
+					<li class='age'><b>Age:</b> %s</li>
+					<li class='grad_year'><b>Graduation Year:</b> %s</li>
+					<li class='bio'><b>Bio:</b> %s</li>
+					<li class='profile_img'><b>Image:</b> %s</li>
+				</ul>",
+				$result["id"],
+				$result["first_name"],
+				$result["last_name"],
+				$result["email"],
+				$result["major"],
+				$result["age"],
+				$result["grad_year"],
+				$result["bio"],
+				$result["profile_img"]);
+		}
+		/* free result set */
+		$results->close();
+	}
 ?>
