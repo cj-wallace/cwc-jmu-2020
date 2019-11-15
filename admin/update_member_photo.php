330055656dbb66e164b08e2cd9ad491cdbc9923b
Connor Wallace <connorjwallace@hotmail.com>
2019-11-15 17:14:30 -0500

    Commit: Fri, Nov 15, 2019  5:14:30 PM

--- admin/update_member_photo.php
+++ admin/update_member_photo.php
@@ -1,10 +1,13 @@
 <?php 
 	require("../include/config.php"); 
 	if (isset($_POST['member_id'])){
-		$query = "UPDATE `member` SET `profile_img` = '{$image}' WHERE `id` = '{$member_id}'";
+		
+		$member_img = addslashes(file_get_contents($_POST['image']['tmp_name']));
+		
+		$query = "UPDATE `member` SET `profile_img` = '{$member_img}' WHERE `id` = '{$member_id}'";
 		$success = $connection->query($query);
 		
-		printf("%s %s\n",$_POST['member_id'],$_POST['image']);
+		printf("%s %s\n",$_POST['member_id'],$_POST['member_img']);
 		
 		if($success){
 			printf("success\n");
