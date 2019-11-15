e3cfd264a791204324c2a91c75439bd52c0010df
Connor Wallace <connorjwallace@hotmail.com>
2019-11-15 18:34:20 -0500

    Commit: Fri, Nov 15, 2019  6:34:20 PM

--- include/head.php
+++ include/head.php
@@ -6,3 +6,5 @@
 <?php } ?>
 
 <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
