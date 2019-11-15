a569ab8f4aabfe2f64b8aebb5a3e8f2a512c3d17
Connor Wallace <connorjwallace@hotmail.com>
2019-11-15 14:05:22 -0500

    Commit: Fri, Nov 15, 2019  2:05:22 PM

--- admin/test.php
+++ admin/test.php
@@ -1,16 +1,17 @@
-<?php
-   if(isset($_POST['submit']) 
-   {
-      printf("First name: " . $_POST['firstname'] . "<br />\n");
-      printf("Last name: " . $_POST['lastname'] . "<br />\n");
-   }
-?>
+
 <html>
-<body>
-<form action="form.php" method="post">
-   <p>First name: <input type="text" name="firstname" /></p>
-   <p>Last name: <input type="text" name="firstname" /></p>
-   <input type="submit" name="submit" value="Submit" />
-</form>
-</body>
+	<body>
+		<?php
+			 if(isset($_POST['submit']) 
+			 {
+					printf("First name: " . $_POST['firstname'] . "<br />\n");
+					printf("Last name: " . $_POST['lastname'] . "<br />\n");
+			 }
+		?>
+		<form action="form.php" method="post">
+			 <p>First name: <input type="text" name="firstname" /></p>
+			 <p>Last name: <input type="text" name="firstname" /></p>
+			 <input type="submit" name="submit" value="Submit" />
+		</form>
+	</body>
 </html>
