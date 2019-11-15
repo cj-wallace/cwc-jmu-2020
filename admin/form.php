fbf5e75d72c7e5e916152e51b3b6b0875157e89d
Connor Wallace <connorjwallace@hotmail.com>
2019-11-15 14:23:43 -0500

    Commit: Fri, Nov 15, 2019  2:23:43 PM

--- admin/form.php
+++ admin/form.php
@@ -14,13 +14,11 @@ $result = mysqli_query($connection, $query) or die(mysqli_error($connection));
 $count = mysqli_num_rows($result);
 
 if ($count == 1){
-
-//echo "Login Credentials verified";
-echo "<script type='text/javascript'>alert('Login Credentials verified')</script>";
-
-}else{
-echo "<script type='text/javascript'>alert('Invalid Login Credentials')</script>";
-//echo "Invalid Login Credentials";
-}
+		//echo "Login Credentials verified";
+		echo "<script type='text/javascript'>alert('Login Credentials verified')</script>";
+	}else{
+		echo "<script type='text/javascript'>alert('Invalid Login Credentials')</script>";
+		//echo "Invalid Login Credentials";
+	}
 }
 ?>
--- admin/test.php
+++ admin/test.php
@@ -1,30 +1,28 @@
 <!DOCTYPE html >
 <html>
-<head>
-<title>MASHLOG LOGIN FORM</title>
-</head>
+	<head>
+	<title>Admin Login</title>
+	</head>
 <body>
-<div>
-<h3>MASHLOG LOGIN FORM</h3>
-    <form id="login-form" method="post" action="form.php" >
-        <table border="0.5" >
-            <tr>
-                <td><label for="user_id">User Name</label></td>
-                <td><input type="text" name="user_id" id="user_id"></td>
-            </tr>
-            <tr>
-                <td><label for="user_pass">Password</label></td>
-                <td><input type="password" name="user_pass" id="user_pass"></input></td>
-            </tr>
-			
-            <tr>
-				
-                <td><input type="submit" value="Submit" />
-                <td><input type="reset" value="Reset"/>
-				
-            </tr>
-        </table>
-    </form>
-		</div>
+	<div>
+		<h3>Login Form</h3>
+		<form id="login-form" method="post" action="form.php" >
+			<table border="0.5" >
+				<tr>
+					<td><label for="user_id">User Name</label></td>
+					<td><input type="text" name="user_id" id="user_id"></td>
+				</tr>
+				<tr>
+					<td><label for="user_pass">Password</label></td>
+					<td><input type="password" name="user_pass" id="user_pass"></input></td>
+				</tr>
+
+				<tr>
+					<td><input type="submit" value="Submit" />
+					<td><input type="reset" value="Reset"/>
+				</tr>
+			</table>
+		</form>
+	</div>
 </body>
 </html>
