001b68d4ec10bb8d3bbddfb33732ee59a7d6a8dc
Connor Wallace <connorjwallace@hotmail.com>
2019-11-15 13:50:13 -0500

    Commit: Fri, Nov 15, 2019  1:50:13 PM

--- admin/index.php
+++ admin/index.php
@@ -33,6 +33,17 @@
 		?>
 	</div>
 
+	<div>
+		<h2>Registration Form</h2>
+
+    <form action="registration_form.php" method="POST"> 
+			First name: <input type="text" name="firstname">
+			<br> 
+			Last name: <input type="text" name="lastname"> 
+			<input type="hidden" name="form_submitted" value="1" /><input type="submit" value="Submit">
+    </form>
+	</div>
+
 	<?php include(PATH_ROOT . "/include/footer.php");?>
 
 </body>
