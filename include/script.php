13f8041858265f51090977d6dbfe578854c06f9f
Connor Wallace <47456687+cj-wallace@users.noreply.github.com>
2020-02-10 02:08:11 -0500

    changes

--- include/script.php
+++ include/script.php
@@ -1,3 +1,3 @@
-<script src= "/include/assets/js/jquery.min.js"></script>
-<script src="/include/assets/bootstrap/js/bootstrap.min.js"></script>
-<script src="/include/assets/js/clean-blog.js"></script>
+<script src= "/assets/js/jquery.min.js"></script>
+<script src="/assets/bootstrap/js/bootstrap.min.js"></script>
+<script src="/assets/js/clean-blog.js"></script>
