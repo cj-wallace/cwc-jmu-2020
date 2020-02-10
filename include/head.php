4bc460244385f92ddf30d0cd1035aa1737b6f4ce
Connor Wallace <47456687+cj-wallace@users.noreply.github.com>
2020-02-10 02:22:26 -0500

    Update head.php

--- include/head.php
+++ include/head.php
@@ -4,8 +4,8 @@
 <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,600italic,700italic,800italic,400,300,600,700,800">
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lora:400,700,400italic,700italic">
-<link rel="stylesheet" href="assets/fonts/font-awesome.min.css">
-<link rel="stylesheet" href="assets/css/Login-Form-Clean.css">
-<link rel="stylesheet" href="assets/css/Navigation-Clean.css">
-<link rel="stylesheet" href="assets/css/Projects-Clean.css">
-<link rel="stylesheet" href="assets/css/Projects-Horizontal.css">
+<link rel="stylesheet" href="/include/assets/fonts/font-awesome.min.css">
+<link rel="stylesheet" href="/include/assets/css/Login-Form-Clean.css">
+<link rel="stylesheet" href="/include/assets/css/Navigation-Clean.css">
+<link rel="stylesheet" href="/include/assets/css/Projects-Clean.css">
+<link rel="stylesheet" href="/include/assets/css/Projects-Horizontal.css">
