16c7d1f1dc8cc7c9dc1aac1f4e61e6b87fc85b4c
Connor Wallace <47456687+cj-wallace@users.noreply.github.com>
2020-02-10 02:41:35 -0500

    Fix home link

--- include/navigation.php
+++ include/navigation.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-light navbar-expand-lg fixed-top" id="mainNav">
-    <div class="container"><a class="navbar-brand" href="index.php">JMU CWC</a><button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
+    <div class="container"><a class="navbar-brand" href="/">JMU CWC</a><button data-toggle="collapse" data-target="#navbarResponsive" class="navbar-toggler" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><i class="fa fa-bars"></i></button>
         <div
                 class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="nav navbar-nav ml-auto">
